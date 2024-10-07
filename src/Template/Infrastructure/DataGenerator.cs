a9409bf907e46f4ce58ba30b16e57e44840f500b
Marawan89 <114577105+Marawan89@users.noreply.github.com>
2024-10-07 22:45:19 +0200

    credentials updated

--- src/Template/Infrastructure/DataGenerator.cs
+++ src/Template/Infrastructure/DataGenerator.cs
@@ -18,7 +18,7 @@ namespace Template.Infrastructure
                 new User
                 {
                     Id = Guid.Parse("3de6883f-9a0b-4667-aa53-0fbc52c4d300"), // Forced to specific Guid for tests
-                    Email = "admin@sunstorage.it",
+                    Email = "andrea@sunstorage.it",
                     Password = "M0Cuk9OsrcS/rTLGf5SY6DUPqU2rGc1wwV2IL88GVGo=", // SHA-256 of text "Prova"
                     FirstName = "Andrea",
                     LastName = "Bianchi",
@@ -27,8 +27,8 @@ namespace Template.Infrastructure
                 new User
                 {
                     Id = Guid.Parse("a030ee81-31c7-47d0-9309-408cb5ac0ac7"), // Forced to specific Guid for tests
-                    Email = "adminfull@sunstorage.it",
-                    Password = "Uy6qvZV0iA2/drm4zACDLCCm7BE9aCKZVQ16bg80XiU=", // SHA-256 of text " "
+                    Email = "marawan@sunstorage.it",
+                    Password = "Uy6qvZV0iA2/drm4zACDLCCm7BE9aCKZVQ16bg80XiU=", // SHA-256 of text "Test"
                     FirstName = "Marawan",
                     LastName = "Emad",
                     NickName = "AdminFull"
