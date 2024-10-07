c7af386fa4fc1d59d6187285ac8e0dafa831ff87
Marawan89 <114577105+Marawan89@users.noreply.github.com>
2024-10-07 18:50:14 +0200

    structure done

--- src/Template/Infrastructure/DataGenerator.cs
+++ src/Template/Infrastructure/DataGenerator.cs
@@ -18,29 +18,20 @@ namespace Template.Infrastructure
                 new User
                 {
                     Id = Guid.Parse("3de6883f-9a0b-4667-aa53-0fbc52c4d300"), // Forced to specific Guid for tests
-                    Email = "email1@test.it",
+                    Email = "admin@sunstorage.it",
                     Password = "M0Cuk9OsrcS/rTLGf5SY6DUPqU2rGc1wwV2IL88GVGo=", // SHA-256 of text "Prova"
-                    FirstName = "Nome1",
-                    LastName = "Cognome1",
-                    NickName = "Nickname1"
+                    FirstName = "Andrea",
+                    LastName = "Bianchi",
+                    NickName = "Admin"
                 },
                 new User
                 {
                     Id = Guid.Parse("a030ee81-31c7-47d0-9309-408cb5ac0ac7"), // Forced to specific Guid for tests
-                    Email = "email2@test.it",
-                    Password = "Uy6qvZV0iA2/drm4zACDLCCm7BE9aCKZVQ16bg80XiU=", // SHA-256 of text "Test"
-                    FirstName = "Nome2",
-                    LastName = "Cognome2",
-                    NickName = "Nickname2"
-                },
-                new User
-                {
-                    Id = Guid.Parse("bfdef48b-c7ea-4227-8333-c635af267354"), // Forced to specific Guid for tests
-                    Email = "email3@test.it",
-                    Password = "Uy6qvZV0iA2/drm4zACDLCCm7BE9aCKZVQ16bg80XiU=", // SHA-256 of text "Test"
-                    FirstName = "Nome3",
-                    LastName = "Cognome3",
-                    NickName = "Nickname3"
+                    Email = "adminfull@sunstorage.it",
+                    Password = "Uy6qvZV0iA2/drm4zACDLCCm7BE9aCKZVQ16bg80XiU=", // SHA-256 of text " "
+                    FirstName = "Marawan",
+                    LastName = "Emad",
+                    NickName = "AdminFull"
                 });
 
             context.SaveChanges();
