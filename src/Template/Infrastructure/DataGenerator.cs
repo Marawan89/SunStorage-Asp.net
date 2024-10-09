53b25058cb1422533a05bbe356f71b239aa4d103
Marawan89 <114577105+Marawan89@users.noreply.github.com>
2024-10-09 17:53:50 +0200

    Update DataGenerator.cs

--- src/Template/Infrastructure/DataGenerator.cs
+++ src/Template/Infrastructure/DataGenerator.cs
@@ -7,6 +7,32 @@ public class DataGenerator
 {
     public static void InitializeDevices(TemplateDbContext context)
     {
+        if (context.Users.Any())
+        {
+            return;   // Data was already seeded
+        }
+
+        context.Users.AddRange(
+            new User
+            {
+                Id = Guid.Parse("3de6883f-9a0b-4667-aa53-0fbc52c4d300"), // Forced to specific Guid for tests
+                Email = "marawan@sunstorage.it",
+                Password = "M0Cuk9OsrcS/rTLGf5SY6DUPqU2rGc1wwV2IL88GVGo=", // SHA-256 of text "Prova"
+                FirstName = "Marawan",
+                LastName = "Emad",
+                NickName = "Marawan"
+            },
+            new User
+            {
+                Id = Guid.Parse("a030ee81-31c7-47d0-9309-408cb5ac0ac7"), // Forced to specific Guid for tests
+                Email = "andrea@sunstorage.it",
+                Password = "Uy6qvZV0iA2/drm4zACDLCCm7BE9aCKZVQ16bg80XiU=", // SHA-256 of text "Test"
+                FirstName = "Andrea",
+                LastName = "Bianchi",
+                NickName = "Andre"
+            });
+        context.SaveChanges();
+
         if (context.Devices.Any())
         {
             return;   // Data was already seeded
